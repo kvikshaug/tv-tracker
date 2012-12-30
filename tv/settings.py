50e9853c437d6d9d428cd5715cf5ace70ab9f316
Ali Kaafarani <ali@kvikshaug.no>
2012-12-30 14:57:58 +0100

    Initial tv project commit

---
+++ tv/settings.py
@@ -0,0 +1,88 @@
+from tv.local_settings import *
+
+TEMPLATE_DEBUG = DEBUG = True
+
+MANAGERS = ADMINS = (
+    ('Ali Kaafarani', 'ali@kvikshaug.no'),
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'tv.db',
+    }
+}
+
+SITE_ID = 1
+TIME_ZONE = 'Europe/Oslo'
+LANGUAGE_CODE = 'nb' # See http://www.i18nguy.com/unicode/language-identifiers.html
+USE_I18N = True
+USE_L10N = True
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    "/home/murray/code/projects/tv/static",
+)
+
+TEMPLATE_DIRS = (
+    "/home/murray/code/projects/tv/templates",
+)
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'tv.urls'
+
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'tv.wsgi.application'
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Uncomment the next line to enable the admin:
+    # 'django.contrib.admin',
+    # Uncomment the next line to enable admin documentation:
+    # 'django.contrib.admindocs',
+)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
