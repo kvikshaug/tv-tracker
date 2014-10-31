14bd3f33c168c9cae3df725dbd5c4f2728914095
Ali Kaafarani <ali@kvikshaug.no>
2014-10-31 18:53:32 +0100

    Save last update date on successful synchronization

--- core/management/commands/sync_all_shows.py
+++ core/management/commands/sync_all_shows.py
@@ -1,8 +1,10 @@
 # encoding: utf-8
+from datetime import datetime
+
 from django.core.management.base import BaseCommand
 
 from core import tvdb
-from core.models import Show
+from core.models import LastUpdate, Show
 
 class Command(BaseCommand):
     args = u''
@@ -11,3 +13,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for show in Show.objects.all():
             tvdb.create_or_update_show(show.tvdbid)
+        last_update = LastUpdate.objects.get()
+        last_update.date = datetime.now()
+        last_update.save()
