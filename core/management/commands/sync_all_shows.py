223d26ac7d9ff79cc1d0bd1591d28b86a6877a4d
Ali Kaafarani <ali@kvikshaug.no>
2015-04-07 19:32:53 +0200

    Fix overlooked reference after model rename

--- core/management/commands/sync_all_shows.py
+++ core/management/commands/sync_all_shows.py
@@ -4,14 +4,14 @@ from datetime import datetime
 from django.core.management.base import BaseCommand
 
 from core import tvdb
-from core.models import LastUpdate, Show
+from core.models import LastUpdate, Series
 
 class Command(BaseCommand):
     args = u''
     help = u"Sync all shows"
 
     def handle(self, *args, **options):
-        for show in Show.objects.all():
+        for show in Series.objects.all():
             tvdb.create_or_update_show(show.tvdbid)
         last_update = LastUpdate.objects.get()
         last_update.date = datetime.now()
