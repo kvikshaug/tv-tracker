fffa9b0a793d87bbdb05ba0bc0e040ed1042ad98
Ali Kaafarani <ali@kvikshaug.no>
2016-03-07 16:34:08 +0100

    Separate prod/dev settings

--- project/settings.py
+++ project/settings.py
@@ -1,67 +1,20 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+_configuration = os.environ.get('DJANGO_CONFIGURATION', 'production').lower()
 
-from project.local_settings import *
+#
+# Note that the order of module imports is important as each module needs boilerplate imports of preceding settings
+#
 
-ROOT_URLCONF = 'project.urls'
-ALLOWED_HOSTS = ['tv.kvikshaug.no']
-WSGI_APPLICATION = 'project.wsgi.application'
-STATIC_URL = '/static/'
+# Always set production settings
+from project.settings_prod import *
 
-TIME_ZONE = 'Europe/Oslo'
-LANGUAGE_CODE = 'en-us' # See http://www.i18nguy.com/unicode/language-identifiers.html
-USE_I18N = True
-USE_L10N = True
+# Override with development settings if running dev-configuration
+if _configuration == 'development':
+    from project.settings_dev import *
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-TEMPLATES = [{
-    'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [os.path.join(BASE_DIR, 'templates')],
-    'APP_DIRS': True,
-    'OPTIONS': {
-        'context_processors': [
-            'django.contrib.auth.context_processors.auth',
-            'django.template.context_processors.debug',
-            'django.template.context_processors.i18n',
-            'django.template.context_processors.media',
-            'django.template.context_processors.static',
-            'django.template.context_processors.tz',
-            'django.contrib.messages.context_processors.messages',
-
-            'project.context_processors.last_update',
-        ],
-    },
-}]
-
-MIDDLEWARE_CLASSES = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-INSTALLED_APPS = [
-    # TODO split prod/dev settings
-    'werkzeug_debugger_runserver',
-
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    'raven.contrib.django',
-    'core',
-]
+# Replace Django's logging configuration completely by disabling it and manually setting our log configuration
+# See http://stackoverflow.com/a/22336174/302484
+import logging.config
+logging.config.dictConfig(LOGGING)
+LOGGING_CONFIG = None
---
+++ project/settings_prod.py
@@ -0,0 +1,115 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+ROOT_URLCONF = 'project.urls'
+WSGI_APPLICATION = 'project.wsgi.application'
+ALLOWED_HOSTS = ['tv.kvikshaug.no']
+SECRET_KEY = os.environ['SECRET_KEY']
+
+TIME_ZONE = 'Europe/Oslo'
+LANGUAGE_CODE = 'en-us' # See http://www.i18nguy.com/unicode/language-identifiers.html
+USE_I18N = True
+USE_L10N = True
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+RAVEN_CONFIG = {
+    'dsn': os.environ['RAVEN_DSN'],
+}
+
+TVDB_API_KEY = os.environ['TVDB_API_KEY']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [os.path.join(BASE_DIR, 'templates')],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            'django.contrib.auth.context_processors.auth',
+            'django.template.context_processors.debug',
+            'django.template.context_processors.i18n',
+            'django.template.context_processors.media',
+            'django.template.context_processors.static',
+            'django.template.context_processors.tz',
+            'django.contrib.messages.context_processors.messages',
+
+            'project.context_processors.last_update',
+        ],
+    },
+}]
+
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'raven.contrib.django',
+    'core',
+]
+
+LOGGING = {
+    'version': 1,
+    # Never disable existing loggers, that will silence logs you want
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s',
+        },
+        'verbose': {
+            'format': '%(levelname)s (%(name)s) %(asctime)s\n%(pathname)s:%(lineno)d in %(funcName)s\n%(message)s\n'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'django.log'),
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        # Djangos DB backend outputs all SQL-statements to DEBUG; limit to INFO
+        'django.db.backends': {
+            'level': 'INFO',
+            'handlers': ['file'],
+            'propagate': False,
+        },
+
+        # Ignore the very verbose template DEBUG statements which include failed context lookups
+        'django.template': {
+            'level': 'INFO',
+            'handles': ['file'],
+            'propagate': False,
+        },
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['file'],
+    }
+}
