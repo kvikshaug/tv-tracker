884c19f6bdfcdb57a33f2e8b011ef364e93e438f
Ali Kaafarani <ali@kvikshaug.no>
2016-03-07 22:47:46 +0100

    Prefetch related objects in series view

--- core/views.py
+++ core/views.py
@@ -27,7 +27,10 @@ def increase_seen(request, series):
     return redirect('core:index')
 
 def series(request, series):
-    series = Series.objects.get(id=series)
+    series = Series.objects.prefetch_related(
+        'seasons',
+        'seasons__episodes',
+    ).get(id=series)
     context = {'series': series}
     return render(request, 'series.html', context)
 
