38a686e52c6aece2eca5d9098004e8922906591b
Ali Kaafarani <ali@kvikshaug.no>
2016-03-08 09:08:53 +0100

    Remove unused code

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,5 @@
 $(document).ready(function() {
-    var footer = $("footer");
-
-    footer.find("a.sync").click(function(e) {
-        if(!confirm("Are you sure!?")) {
-            e.preventDefault();
-        }
-    });
-
     $("h4.season").click(function() {
         $("table.episodes[data-season='" + $(this).attr('data-season') + "']").toggle();
     });
-
 });
