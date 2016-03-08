ce4c8b22ba765c487eea3bac9b2c0269f50294bd
Ali Kaafarani <ali@kvikshaug.no>
2016-03-08 12:54:06 +0100

    Build with docker-compose and tag the resulting image

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,11 @@
 # Ensure css is compiled before creating the image
 docker-compose run --rm builder sass --scss --update scss:css
 
-docker build -t eu.gcr.io/monkey-island-1227/tv.kvikshaug.no .
+IMAGE_NAME=${PWD##*/}_app
+docker-compose build app
+docker tag ${IMAGE_NAME} kvikshaug/tv.kvikshaug.no
+docker tag ${IMAGE_NAME} eu.gcr.io/monkey-island-1227/tv.kvikshaug.no
+
 gcloud docker push eu.gcr.io/monkey-island-1227/tv.kvikshaug.no
 
 # can't use rolling-update with rw GCE PD
