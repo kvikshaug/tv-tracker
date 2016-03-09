4d173c6bb6b1dfc984bb58cf97e88749c4bc364c
Ali Kaafarani <ali@kvikshaug.no>
2016-03-09 12:43:43 +0100

    Set default image name

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
+IMAGE_NAME="kvikshaug/tv.kvikshaug.no"
 
 # Ensure css is compiled before creating the image
 docker-compose run --rm builder sass --scss --update scss:css
 
-IMAGE_NAME=${PWD##*/}_app
 docker-compose build app
-docker tag ${IMAGE_NAME} kvikshaug/tv.kvikshaug.no
 docker tag ${IMAGE_NAME} eu.gcr.io/monkey-island-1227/tv.kvikshaug.no
-
 gcloud docker push eu.gcr.io/monkey-island-1227/tv.kvikshaug.no
 
 # can't use rolling-update with rw GCE PD
