988c3f41e973e69a951b4620a65361db27780a02
Ali Kaafarani <ali@kvikshaug.no>
2016-03-09 15:31:38 +0100

    Move API XML parsing to tvdb models

--- thetvdb/models.py
+++ thetvdb/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class SeriesSearchResult():
     def __init__(self, id, name, overview, banner, first_aired, imdb):
         self.id = id
@@ -9,6 +11,23 @@ class SeriesSearchResult():
 
     def __str__(self):
         return "%s: %s" % (self.id, self.name)
+
+    @staticmethod
+    def from_xml(xml):
+        # Required fields (i.e. throw exception if missing)
+        id = xml.find("seriesid").text
+        name = xml.find("SeriesName").text
+
+        # Optional fields
+        overview = xml.findtext("Overview", default="")
+        banner = xml.findtext("banner", default="")
+        try:
+            first_aired = datetime.strptime(xml.findtext("FirstAired", default=""), "%Y-%m-%d")
+        except ValueError:
+            first_aired = None
+        imdb = xml.findtext("IMDB_ID", default="")
+
+        return SeriesSearchResult(id, name, overview, banner, first_aired, imdb)
 
 class Series():
     def __init__(self, tvdbid, name, overview, status, banner, first_aired, imdb):
@@ -24,8 +43,51 @@ class Series():
     def __str__(self):
         return "%s (tvdb id: %s)" % (self.name, self.tvdbid)
 
-    def add_episode(self, season):
-        self.episodes.append(season)
+    @staticmethod
+    def from_xml(xml):
+        series = xml.find("Series")
+        tvdbid = series.findtext("id", default="")
+        name = series.findtext("SeriesName", default="")
+        overview = series.findtext("Overview", default="")
+        status = series.findtext("Status", default="")
+        banner = series.findtext("banner", default="")
+        try:
+            first_aired = datetime.strptime(series.findtext("FirstAired", default=""), "%Y-%m-%d")
+        except ValueError:
+            first_aired = None
+        imdb = series.findtext("IMDB_ID", default="")
+
+        series = Series(
+            tvdbid=tvdbid,
+            name=name,
+            overview=overview,
+            status=status,
+            banner=banner,
+            first_aired=first_aired,
+            imdb=imdb,
+        )
+
+        for e in xml.findall("Episode"):
+            season = e.findtext("SeasonNumber", default="")
+            if season == "0":
+                # Ignore specials for now
+                continue
+
+            number = e.findtext("EpisodeNumber", default="")
+            first_aired = e.findtext("FirstAired", default="")
+
+            try:
+                first_aired = datetime.strptime(first_aired, "%Y-%m-%d")
+            except (ValueError, TypeError):
+                first_aired = None
+
+            series.episodes.append(Episode(
+                season=season,
+                number=number,
+                first_aired=first_aired,
+            ))
+
+        return series
 
 class Episode():
     def __init__(self, season, number, first_aired):
--- thetvdb/tvdb.py
+++ thetvdb/tvdb.py
@@ -1,19 +1,18 @@
 from django.conf import settings
 
-from datetime import datetime
 from io import BytesIO
 from xml.etree import ElementTree
 import zipfile
 
 import requests
 
-from .models import SeriesSearchResult, Series as TVDBSeries, Episode
+from .models import SeriesSearchResult, Series as TVDBSeries
 from core.models import Series
 
 def search_for_series(query):
     content = requests.get("%s/GetSeries.php?seriesname=%s" % (settings.TVDB_API_ENDPOINT, query)).content
     xml = ElementTree.fromstring(content)
-    return [parse_search_result(series_xml) for series_xml in xml.findall("Series")]
+    return [SeriesSearchResult.from_xml(series_xml) for series_xml in xml.findall("Series")]
 
 def create_or_update_series(tvdbid):
     # TODO: Remove stale episode objects
@@ -25,7 +24,7 @@ def create_or_update_series(tvdbid):
     content = zipfile.ZipFile(BytesIO(zipped)).read("en.xml")
     xml = ElementTree.fromstring(content)
 
-    series_data = parse_series(xml)
+    series_data = TVDBSeries.from_xml(xml)
 
     series, created = Series.objects.update_or_create(tvdbid=series_data.tvdbid, defaults={
         'name': series_data.name,
@@ -40,71 +39,5 @@ def create_or_update_series(tvdbid):
         episode, created = season.episodes.get_or_create(number=episode_data.number)
         episode.air_date = episode_data.first_aired
         episode.save()
-
-    return series
-
-
-#
-# Parsing and utilities
-#
-
-def parse_search_result(xml):
-    # Required fields (i.e. throw exception if missing)
-    id = xml.find('seriesid').text
-    name = xml.find('SeriesName').text
-
-    # Optional fields
-    overview = xml.findtext('Overview', default='')
-    banner = xml.findtext('banner', default='')
-    try:
-        first_aired = datetime.strptime(xml.findtext('FirstAired', default=''), "%Y-%m-%d")
-    except ValueError:
-        first_aired = None
-    imdb = xml.findtext('IMDB_ID', default='')
-
-    return SeriesSearchResult(id, name, overview, banner, first_aired, imdb)
-
-def parse_series(xml):
-    series = xml.find("Series")
-    tvdbid = series.findtext("id", default='')
-    name = series.findtext("SeriesName", default='')
-    overview = series.findtext("Overview", default='')
-    status = series.findtext("Status", default='')
-    banner = series.findtext("banner", default='')
-    try:
-        first_aired = datetime.strptime(series.findtext("FirstAired", default=''), "%Y-%m-%d")
-    except ValueError:
-        first_aired = None
-    imdb = series.findtext("IMDB_ID", default='')
-
-    series = TVDBSeries(
-        tvdbid=tvdbid,
-        name=name,
-        overview=overview,
-        status=status,
-        banner=banner,
-        first_aired=first_aired,
-        imdb=imdb,
-    )
-
-    for e in xml.findall("Episode"):
-        season = try_field(e, "SeasonNumber")
-        if season == '0':
-            # Ignore specials for now
-            continue
-
-        number = try_field(e, "EpisodeNumber")
-        first_aired = try_field(e, "FirstAired")
-
-        try:
-            first_aired = datetime.strptime(first_aired, "%Y-%m-%d")
-        except (ValueError, TypeError):
-            first_aired = None
-
-        series.add_episode(Episode(
-            season=season,
-            number=number,
-            first_aired=first_aired,
-        ))
 
     return series
