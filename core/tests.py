97c4e50acbeef9ed1bf68df10115a714c43a0686
Ali Kaafarani <ali@kvikshaug.no>
2016-03-09 16:55:14 +0100

    Test / URL

---
+++ core/tests.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from django.test import TestCase, Client
+
+from core.models import LastUpdate
+
+class ViewsTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        LastUpdate.objects.create(datetime=datetime(2016, 1, 1, 12, 0, 0))
+
+    def test_get_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
